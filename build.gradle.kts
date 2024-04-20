3c310128fe7c13d9160a1f8c2899e4ce5b315b2e
Saturn <90934664+Saturn745@users.noreply.github.com>
2024-04-20 03:00:09 +0000

    Swap Github Packages for Codeberg Packages
    Main reason for this is so people don't have to deal with Github's forced authentication. It also seems adding Jitpack is not needed with this :thinking: Coincidence?

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,17 +8,14 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://repo.purpurmc.org/snapshots")
-    maven("https://jitpack.io")
-    maven {
-        url = uri("https://maven.pkg.github.com/lualink/LuaLink") // TODO: Change to the LuaLink organization
-    }
+    maven("https://codeberg.org/api/packages/LuaLink/maven")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT")
-    compileOnly("xyz.galaxyy.lualink:lualink:1.20.1-10")
+    compileOnly("xyz.galaxyy.lualink:lualink:1.20.4-SNAPSHOT")
 }
 
 tasks.test {
